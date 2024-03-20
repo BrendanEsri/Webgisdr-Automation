ecf15736dfd3575b2bfa593da9f85037d9d5b5bb
brendanbladdick-adot <BBladdick@esri.com>
2024-03-20 11:03:36 -0600

    final - working

--- webgisdr_retention_previousbackups_daily_monthly.ps1
+++ webgisdr_retention_previousbackups_daily_monthly.ps1
@@ -65,21 +65,21 @@ $backupDirectory = "C:\webgisdr\Backup" # Path to the WebGIS DR backup folder
 $dailyBackups = "C:\webgisdr\Retention_Daily" # Path to the daily backup folder
 $monthlyBackups = "C:\webgisdr\Retention_Monthly" # Path to the monthly backup folder
 $batFile = "C:\webgisdr\webgisdr_export.bat" # Path to the WebGIS DR batch file
-$days = 2  # Number of days to retain daily backups
+$days = 3  # Number of days to retain daily backups
 $months = 4 # Number of months to retain monthly backups
 
 # ArcGIS Enterprise variable
 $environment = "gis.company.com" # Name of the ArcGIS Enterprise Environment being backed up that will be used in the email subject (e.g., gis.company.com)
 
 # Email variables for sending notifications (if using ssl and/or authentication for your SMTP server you can set the $useSSL & $useAuthentication variables to $true and provide the username and password for authentication)
-$smtpServer = "your.smtp.server.com" # Your SMTP server address (e.g., smtp.gmail.com)
+$smtpServer = "smtp.esri.com" # Your SMTP server address (e.g., smtp.gmail.com)
 $smtpPort = 25 # or 587/465 depending on your server settings
 $useSSL = $false # Set to $true if your SMTP server requires SSL
 $useAuthentication = $false # Set to $true if your SMTP server requires authentication
 $smtpUser = "" # Your SMTP username (leave blank if not needed)
 $smtpPassword = "" # Your SMTP password (leave blank if not needed)
-$fromEmail = "fromEmail@example.com" # Your email address
-$toEmail = "toEmail@example.com" # Recipient email address
+$fromEmail = "automatedWebGisDr@esri.com" # Your email address
+$toEmail = "bbladdick@esri.com" # Recipient email address
 
 # Initialize SMTP client for sending emails 
 $smtpClient = New-Object Net.Mail.SmtpClient($smtpServer, $smtpPort)
@@ -90,6 +90,11 @@ if ($useAuthentication -and $smtpUser -ne "" -and $smtpPassword -ne "") {
     $smtpClient.UseDefaultCredentials = $false
 }
 
+# Log file variables
+$logFilePath = "C:\webgisdr\webgisdr.log" # Path to the log file
+$archiveFolderPath = "C:\webgisdr\webgisdr_archive_log" # Path to the archive folder
+$logFileName = [System.IO.Path]::GetFileNameWithoutExtension($logFilePath) # Get the log file name without extension
+$logFileExtension = [System.IO.Path]::GetExtension($logFilePath) # Get the log file extension
 
 # Email variables
 $errorLogFileSubject = "WebGIS DR Process FAILED for the $environment environment" # Email subject
@@ -97,12 +102,6 @@ $errorLogFileBody = "There was an error in the WebGIS DR Process, please review 
 # If the log file is missing, an email will be sent with the following subject and body
 $missingLogFileSubject = "WebGIS DR Process didn't run for the $environment environment" # Email subject
 $missingLogFileBody = "The log file expected at '$logFilePath' does not exist after running the webgisdr process. Please check the process and ensure it's configured to generate a log. May need to review task scheduler history for more details. If the log file is generated in a different location, please update the script accordingly. Feel free to reach out to Esri Professional Services or Esri Technical Support for assistance" # Email body
-
-# Log file variables
-$logFilePath = "C:\webgisdr\webgisdr.log" # Path to the log file
-$archiveFolderPath = "C:\webgisdr\webgisdr_archive_log" # Path to the archive folder
-$logFileName = [System.IO.Path]::GetFileNameWithoutExtension($logFilePath) # Get the log file name without extension
-$logFileExtension = [System.IO.Path]::GetExtension($logFilePath) # Get the log file extension
 
 ############################################################################################################
 ## End Variables.
@@ -138,21 +137,13 @@ try {
             }
             # 1a2b2.) If no file for this month exists in Monthly, copy it to the Monthly folder. Uses copy instead of move to allow the daily retention to be processed after the monthly retention. After these operations are completed successfully, all of the backup files will be removed from the backup directory.
             if (-not $monthlyFile) {
-                Copy-Item -Path $_.FullName -Destination $monthlyBackups -ErrorAction SilentlyContinue -ErrorVariable copyErrorMonth
-                if ($copyErrorMonth) {
-                    # Log or handle the error
-                    Write-Host "Error copying file $($_.FullName) to '$monthlyBackups': $($copyErrorMonth.Exception.Message)"
-                }
+                Copy-Item -Path $_.FullName -Destination $monthlyBackups
             }
         }
 
         ## 1a2c.) Move files to Daily if within $days
         if ($fileDate -ge $cutOffDateDaily) {
-            Move-Item -Path $_.FullName -Destination $dailyBackups -ErrorAction SilentlyContinue -ErrorVariable copyErrorDaily
-            if ($copyErrorDaily) {
-                # Log or handle the error
-                Write-Host "Error moving file $($_.FullName) to '$dailyBackups': $($copyErrorDaily.Exception.Message)"
-            }
+            Move-Item -Path $_.FullName -Destination $dailyBackups
         }
     }
     $operationSuccessful = $true # Set the success flag to true if the above block completes successfully to allow the removal of all files from the backup directory in the next step
@@ -211,13 +202,15 @@ Write-Host "Backup organization and cleanup process completed."
 #### 2a.) Archive the log file so a new one can be created for the next run to properly monitor for errors
 try {
     ### 2a1.) Check if the archive folder exists, if not, create it
-    if (Test-Path $logFilePath) {
-        if (-not (Test-Path $archiveFolderPath)) {
-            New-Item -ItemType Directory -Path $archiveFolderPath # Create the archive folder if it doesn't exist
-        }
+    if (-not (Test-Path $archiveFolderPath)) {
+        New-Item -ItemType Directory -Path $archiveFolderPath # Create the archive folder if it doesn't exist
+    }
 
+    if (Test-Path $logFilePath) {
         ### 2a2.) Create a timestamped archive filename
         $timestamp = Get-Date -Format "yyyy-MM-dd_HHmmss" # Get the current date and time
+        $logFileName = [System.IO.Path]::GetFileNameWithoutExtension($logFilePath) # Ensure you retrieve the log file name
+        $logFileExtension = [System.IO.Path]::GetExtension($logFilePath) # Ensure you retrieve the log file extension
         $archivedLogFileName = "$logFileName" + "_" + $timestamp + "$logFileExtension" # Create the archive file name with timestamp appended to the original file name (e.g., webgisdr_2021-09-30_123456.log)
         $archivedLogFilePath = Join-Path $archiveFolderPath $archivedLogFileName # Create the full path to the archive file
 
@@ -227,7 +220,6 @@ try {
 } catch {
     Write-Host "An error occurred during step #2a: Archive the log file so a new one can be created for the next run to properly monitor for errors - : $_"
 }
-
 
 ############################################################################################################
 ##### End 2.) Archives the previous days WebGIS DR log files into an archive folder so the WebGIS DR process can create a new log that will be monitored for errors or issues and sends an email notification if something goes wrong with the backup process.
@@ -244,7 +236,6 @@ try {
     Write-Host "An error occurred during step #3a: Run the bat file after the log file has been archived - $_"
 }
 
-
 ############################################################################################################
 ##### End 3.) Runs the WebGIS DR process using a batch file after the log file has been archived.
 ############################################################################################################
@@ -252,7 +243,6 @@ try {
 ############################################################################################################
 ##### Start 4.) Set up email notifications for alerting if things go wrong with the WebGIS DR process.
 ############################################################################################################
-
 
 try {
     #### 4a.) If the log file is missing, send an email
@@ -264,6 +254,7 @@ try {
         $missingLogMailMessage.Subject = $missingLogFileSubject # Set the email subject
         $missingLogMailMessage.Body = $missingLogFileBody # Set the email body
         $smtpClient.Send($missingLogMailMessage) # Send the email
+        Write-Host "Log file is missing, an email has been sent to notify the recipient."
     }
     #### 4b.) If there is an error in the log file, send an email
     elseif (Select-String -Path $logFilePath -Pattern "ERROR") { # Check if the log file contains the word "ERROR"
@@ -274,11 +265,11 @@ try {
         $mailMessage.Subject = $errorLogFileSubject # Set the email subject
         $mailMessage.Body = $errorLogFileBody # Set the email body
         $smtpClient.Send($mailMessage) # Send the email
+        Write-Host "An error was found in the log file, an email has been sent to notify the recipient."
     }
 } catch {
     Write-Host "An error occurred during step #4a - #4b: Set up email notifications for alerting if things go wrong with the WebGIS DR process - $_"
 }
-
 
 ############################################################################################################
 ##### End 4.) Set up email notifications for alerting if things go wrong with the WebGIS DR process.
